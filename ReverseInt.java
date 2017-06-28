825e876ed848fd023a2567ff0bd1b170af0cfb46
NivethaPR <nivethapalanisamy.be@gmail.com>
2017-06-28 10:31:11 +0530

    Add files via upload

--- ReverseInt.java
+++ ReverseInt.java
@@ -1,5 +1,12 @@
 package Guvi;
 
 public class ReverseInt {
+	 public int getReverseInt(int value) {
+		    int resultNumber = 0;
+		    for(int i = value; i !=0; i /= 10) {
+		        resultNumber = resultNumber * 10 + i % 10;
+		    }
+		    return resultNumber;        
+		}
 
 }
